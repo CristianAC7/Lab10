bfcd1ffb3483d17378fb3cc3c228ff5e48c6ccdb
ynn <yennycibiris@hotmail.com>
2015-11-09 22:16:39 -0500

    controller

--- src/main/resources/static/js/app/controllers/blogctrl.js
+++ src/main/resources/static/js/app/controllers/blogctrl.js
@@ -28,7 +28,7 @@ app.controller("blogCtrl", function($scope,$log,$http) {
         $log.debug($scope.entry);
         $http({
             method  : 'POST',
-            url     : 'blogs',
+            url     : 'blog',
             data    : $scope.entry
         }).success(function(data) {
             console.log(data);
